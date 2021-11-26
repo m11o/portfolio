94aeca91d47cdd9d1855e6813365a51e7834ca69
Masahito Osako <velonica.gale2227@gmail.com>
2021-11-26 19:08:25 +0900

    add font config

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -2,8 +2,17 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  useSystemColorMode: true
+  useSystemColorMode: true,
 }
 
-const theme = extendTheme({ config })
+const fontConfig = {
+  fonts: {
+    header: 'Dancing Script',
+    body: 'Roboto, Kosugi Maru, Arial, sans-serif',
+    heading: 'Roboto, Kosugi Maru, Arial, sans-serif',
+    mono: 'Roboto, Kosugi Maru, Arial, sans-serif'
+  }
+}
+
+const theme = extendTheme({ config, ...fontConfig })
 export default theme
