2f4b906b1805c796dd0bca4f227b2086198e6a4f
Masahito Osako <velonica.gale2227@gmail.com>
2021-12-02 08:50:04 +0900

    add resume page and component

--- src/components/pages/resume.tsx
+++ src/components/pages/resume.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Resume = () => (
-  <p>hoge</p>
-)
+import ReactMarkdown from 'react-markdown'
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+
+import * as ResumeMarkdownPath from '../../assets/resume.md'
+import markdownTheme from './resume/markdown_theme'
+
+const Resume = () => {
+  const [markdown, setMarkdown] = useState('')
+
+  useEffect(() => {
+    const f = () => {
+      fetch(ResumeMarkdownPath.default)
+        .then(response => response.text())
+        .then(text => setMarkdown(text))
+    }
+    f()
+  })
+
+  return (
+    <ReactMarkdown components={ChakraUIRenderer(markdownTheme)} skipHtml >
+      {markdown}
+    </ReactMarkdown>
+  )
+}
 export default Resume
