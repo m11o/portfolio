09385cc3f8d8600e8277915c1463c57f392d2823
Masahito Osako <velonica.gale2227@gmail.com>
2021-12-07 23:07:22 +0900

    responsive for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { Box, Heading, Stack, Link, useColorModeValue } from '@chakra-ui/react'
+import { Box, Heading, Stack, Link, useColorModeValue, Flex, Menu, MenuButton, MenuList, MenuItem, IconButton } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 type HeaderLinkProps = {
   href: string,
@@ -12,7 +13,7 @@ const HeaderLink: React.FC<HeaderLinkProps> = ({ href, target = '_self', childre
 const Header: React.FC = () => {
   return (
     <Box as='header' w='100%' bg={useColorModeValue('#ffffff40', 'gray.800')} >
-      <Box as='nav' w='100%' px='32px' h='auto'>
+      <Box as='nav' w='100%' px={{ base: 0, md: 8 }} h='auto'>
         <Stack direction={{ base: 'column', md: 'row' }} display={{ base: 'none', md: 'flex' }} justifyContent='end' alignItems='center' flexGrow={1}>
           <HeaderLink href='/'>TOP</HeaderLink>
           <HeaderLink href='/works'>Works</HeaderLink>
@@ -20,6 +21,18 @@ const Header: React.FC = () => {
           <HeaderLink href='/resume'>Resume</HeaderLink>
           <HeaderLink href='https://github.com/m11o' target='_blank'>Github</HeaderLink>
         </Stack>
+        <Box justifyContent='flex-end' display={{ base: 'flex', md: 'none' }} w='100%'>
+          <Menu isLazy>
+            <MenuButton icon={<HamburgerIcon />} as={IconButton} variant='outline' />
+            <MenuList>
+              <MenuItem as={Link} href='/'>TOP</MenuItem>
+              <MenuItem as={Link} href='/works'>Works</MenuItem>
+              <MenuItem as={Link} href='/posts'>Blogs</MenuItem>
+              <MenuItem as={Link} href='/resume'>Resume</MenuItem>
+              <MenuItem as={Link} href='https://github.com/m11o' target='_blank'>Github</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
       </Box>
       <Box>
         <Heading as='h1' textAlign='center' mt={40} mb={20} fontFamily='header' fontWeight='bold'>Love Beautiful Code</Heading>
