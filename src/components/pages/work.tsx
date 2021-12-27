c1edc1b5a8f7e92cef0ae0aec1c3c13db947fbca
Masahito Osako <velonica.gale2227@gmail.com>
2021-12-28 01:12:55 +0900

    fix portfolio bug

--- src/components/pages/work.tsx
+++ src/components/pages/work.tsx
@@ -65,34 +65,33 @@ export default class Work extends React.Component<WorkProps, WorkState> {
     return (
       this.state.works.length || this.state.thumbnailLinks.length ? (
         <>
-          {this.state.works.map((work: EntryFields.Object) => {
-            const { name, thumbnail, libs, link, description } = work.fields
+          <Box w='100%'>
+            <Heading as='h1' size='md' mb={8}>ポートフォリオ</Heading>
+            <Flex direction={{ base: 'column', md: 'row' }} justifyContent='space-between'>
+              {this.state.works.map((work: EntryFields.Object) => {
+                const { name, thumbnail, libs, link, description } = work.fields
 
-            return (
-              <Box key={name} w='100%'>
-                <Heading as='h1' size='md' mb={8}>ポートフォリオ</Heading>
-                <Flex direction='row' justifycontent='space-between'>
-                  <LinkBox as='article' borderWidth='3px' rounded='md' h='auto' maxH={{ base: '400px', md: '450px' }} w={{ base: '100%', md: '45%' }}>
-                    <Image borderbottomwidth='2px' src={thumbnail && thumbnail.sys.id ? (this.state.thumbnailLinks[thumbnail.sys.id]) : PortfolioIcon} bordertopradius='md' h='50%' w='100%' fit='cover' />
+                return (
+                  <LinkBox key={name} as='article' borderWidth='3px' rounded='md' h='auto' maxH={{ base: '400px', md: '500px' }} w={{ base: '100%', md: '45%' }} mb={{ base: 5, md: 0 }}>
+                    <Image borderBottomWidth='2px' src={thumbnail && thumbnail.sys.id ? (this.state.thumbnailLinks[thumbnail.sys.id]) : PortfolioIcon} borderTopRadius='md' h='50%' w='100%' fit='fill' />
                     <Box p={4}>
                       <Heading as='h4' size='md' mb={2}>
-                        <LinkOverlay href={link}>
+                        <LinkOverlay href={link} isExternal>
                           {name}
                         </LinkOverlay>
                       </Heading>
-                      <Text size='sm' nooflines={{ base: 4, md: 3 }}>{description}</Text>
-                      <Flex flexwrap='wrap' mt={4} alignitems='baseline'>
+                      <Text size='sm' noOfLines={{ base: 4, md: 3 }}>{description}</Text>
+                      <Flex flexWrap='wrap' mt={4} alignItems='baseline'>
                         <Heading as='h6' size='sm' mr={1}>使用技術:</Heading>
                         {libs.length ? (
-                          libs.map((lib: string) => <Tag colorscheme='green' size='sm' mr={1} mb={1} key={lib}>{lib}</Tag>)
+                          libs.map((lib: string) => <Tag colorScheme='green' size='sm' mr={1} mb={1} key={lib}>{lib}</Tag>)
                         ) : ''}
                       </Flex>
                     </Box>
                   </LinkBox>
-                </Flex>
-              </Box>
-            )
-          })}
+                )})}
+            </Flex>
+          </Box>
         </>
       ) : (
         <Text fontSize='sm' color='gray.400' textAlign='center'>現在、表示できるPortfolioがありません</Text>
